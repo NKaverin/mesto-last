4c244e829d08e19e490ded17b329e8f4cde3c9ca
NKaverin <87134966+NKaverin@users.noreply.github.com>
2021-12-01 13:37:21 +0300

    2

--- script.js
+++ script.js
@@ -160,9 +160,9 @@ settings = {
     formSelector: '.popup__form',
     inputSelector: '.popup__field',
     submitButtonSelector: '.popup__submit-button',
-    inactiveButtonClass: '.popup__submit-button_inactive',
-    inputErrorClass: '.popup__field_type_error',
-    errorClass: '.popup__field-error_active'
+    inactiveButtonClass: 'popup__submit-button_inactive',
+    inputErrorClass: 'popup__field_type_error',
+    errorClass: 'popup__field-error_active'
 }
 
 // показываем ошибку по полю
